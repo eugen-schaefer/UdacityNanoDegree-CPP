15d6b8781efdb1c8c0f946080cb1371757400da6
Eugen Schaefer <eugen.schaefer1979@gmail.com>
2020-10-14 22:55:22 +0200

    - minor reordering in src/TrafficLight.h

--- ConcurrentTrafficSimulation/src/TrafficLight.h
+++ ConcurrentTrafficSimulation/src/TrafficLight.h
@@ -46,11 +46,11 @@ public:
     TrafficLight();
 
     // getters / setters
-
+    TrafficLightPhase getCurrentPhase();
+    
     // typical behaviour methods
     void waitForGreen();
     void simulate();
-    TrafficLightPhase getCurrentPhase();
 
 private:
     // typical behaviour methods
